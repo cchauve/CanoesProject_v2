86c9b54850bc3c70aa5ba04bfcee548a22043a1a
HarutoMatsui <110897345+HarutoMatsui@users.noreply.github.com>
2022-08-18 08:18:22 -0700

    Update plot_functions_haruto.py

--- canoe-project-phase1/scripts/plot_functions_haruto.py
+++ canoe-project-phase1/scripts/plot_functions_haruto.py
@@ -1,10 +1,19 @@
-#import numpy, which is useful to plot functions
-#import plotly.graph_objs, which is used to make 3D graphs
 import numpy as np
 import plotly.graph_objs as go
+
 def coordinates(w, h, l, c, type):
-    #This is to calculate the 3D coordinates of the Canoe
-    #it returns 2D list of numbers X, Y, Z that stores 3D coordinates.
+    """
+    Get the 3D coordinates of the canoe
+    input: 5 integer values for width, height, length, corner radius, and canoe type
+    output: list of x-coordinates of the canoe, named X
+    output: list of y-coordinates of the canoe, named Y
+    output: list of z-coordinates of the canoe, named Z
+    """
+    
+    """
+    get two arrays of numbers a and b for each canoe type to do the calculation
+    calculate x-coordinates, y-coordinates, z-coordinates of the canoe
+    """
     if  type == 1:
         a, b = np.meshgrid(np.linspace(0.0, 2*np.pi, 50), np.linspace(np.pi/2, (3/2)*np.pi, 50))
         X = l*np.cos(a)
@@ -25,14 +34,17 @@ def coordinates(w, h, l, c, type):
         return X, Y, Z
 
 def plot_canoe(width, height, length, corner_radius, type):
-    #For the input, we have 4 array input for 'width, height, length, and corner_radius'
-    #and 1 integer input for 'type'.
+    """
+    make a 3D figure of a canoe. The figure can only show one slider, and accepts only one changing variable
+    input: 4 arrays for width, height, length, and corner radius of the canoe
+    input: 1 integer for canoe type
+    output: 3D plot of the canoe with a changing variable
+    """
     
-    #make a figure of a canoe we want to make, and returns the figure we made.
-    #the figure can only show one changing parameter for the slider.
-    
-    #first, find which parameter is the changing parameter, 
-    #and put that paraneter name into 'label'
+    """
+    first, find which parameter is the changing variable
+    so we could use that variable name in the figure
+    """
     which_R_max = np.argmax([len(width), len(height), len(length), len(corner_radius)])
     if which_R_max == 0:
         R_selected = width
@@ -47,28 +59,28 @@ def plot_canoe(width, height, length, corner_radius, type):
         R_selected = corner_radius
         label = 'corner radius: '
     
-    #Then, get the min and max range of the 3D figure of the canoe we will be making
+    """
+    then, get the min and max range of the 3D figure of the canoe we will be making
+    """
     max_range = 1.5 * max([max(width), max(height), max(length)])
     min_range = - max_range
 
     fig = go.Figure()
 
-    #Since we want to make a canoe with a changing parameter
-    #we will be making a canoe for all the cases parameter changes
-    #and switch which canoe to show as we decide on which parameter to show. 
+    """
+    since we want to make a canoe with a changing variable
+    we will be making a canoe for all the cases value changes
+    and switch which canoe to show as we decide on which value to show
+    """
     
     for w in width:
             for h in height:
                 for l in length:
                     for c in corner_radius:
-                        #Do calculations to get the coordinates to make the 
-                        #3D figure according to the type of Canoe we have.
-                        #X is the list of X coordinates of the canoe we are making
-                        #Y is the list of Y coordinates of the canoe we are making
-                        #Z is the list of Z coordinates of the canoe we are making
+                        """
+                        make the 3D figure for each values in changing variable.
+                        """
                         X, Y, Z = coordinates(w, h, l, c, type)
-                        
-                        #make the 3D figure
                         fig.add_trace(go.Surface(x=X, y=Y, z=Z, visible=False))
                         fig.layout = go.Layout(title='Type '+str(type)+' canoe visualization',
                             scene=dict(
@@ -79,12 +91,12 @@ def plot_canoe(width, height, length, corner_radius, type):
     fig.update_yaxes(scaleanchor = "x", scaleratio = 1)
     fig.data[0].visible = True
 
-#Make a slider for the changing parameter
+    """
+    make a slider for the changing variable
+    """
     steps = []
     for i in range(len(fig.data)):
-        step = dict(method="update", label="{:.2f}".format(R_selected[i]),
-                    args=[{"visible": [False] * len(fig.data)}])
-        step["args"][0]["visible"][i] = True
+        step = dict(label="{:.2f}".format(R_selected[i]), args=[{"visible": [False] * len(fig.data)}])
         steps.append(step)
 
     sliders = [dict(active=0, currentvalue={"prefix": label}, pad={"t": 50},
