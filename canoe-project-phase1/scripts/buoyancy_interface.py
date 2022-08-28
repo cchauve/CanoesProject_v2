039b0633991044eef19ba94acc35f690542ba6bf
Kaj Wilson <47764116+HitchyHawk@users.noreply.github.com>
2022-08-27 21:59:27 -0700

    Figured out how "@update" works
    fiddled with diagrams a bit, wrote a bit more.

--- canoe-project-phase1/scripts/buoyancy_interface.py
+++ canoe-project-phase1/scripts/buoyancy_interface.py
@@ -7,9 +7,13 @@ WILL SWITCH TO WHAT HARUTO IS USING IN THE FUTURE
 """
 import plotly.graph_objects as go
 import numpy as np
+import ipywidgets as widgets
+from ipywidgets import *
+import decimal
+
 import scripts.cubeModel as cm
-import scripts.buoyancy_equilibrium as eq #not being used yet
-import plot_funcs as pf
+#import scripts.buoyancy_equilibrium as eq #not being used yet
+#import plot_funcs as pf
 
 GLOBAL_WaterDensity = 997     #(kg / m^3)
 GLOBAL_Gravity      = 9.80665 #(m/s^2)
@@ -58,7 +62,45 @@ def EquilibriumCubeGraph(length = 5, depth = 5, height = 5, resolution = 32):
     """
     An interactive graph with slider for weight. Shows where the equilibrium of the cube is.
     Subgraph on the side plotting weight vs equilibrium.
-    """
+    """     
+    fig = go.Figure()
+    
+    fig.update_layout(
+        showlegend = False,
+        width = 500,
+        height = 700
+    )
+    fig.update_xaxes(range = [length  * (-1/4), length * 5/4])
+    fig.update_yaxes(range = [height  * (-5/4), height * 5/4], scaleanchor = "x", scaleratio = 1)
+    
+    ##initial plot
+    waterHeight = cm.CalcEquilibrium(height, 0, GLOBAL_WaterDensity, GLOBAL_Gravity)
+    
+    traceCube = cm.GetCubeTrace(length, height, waterHeight)
+    traceWater = cm.GetWaterTrace(length, waterHeight)
+    
+    fig.add_trace(traceCube)
+    fig.add_trace(traceWater)
+    
+    fig.data[0].visible = True
+    fig.data[1].visible = True
+    #fig.show()
+    
+    maximum_density = GLOBAL_WaterDensity/ GLOBAL_Gravity
+    stepsize = maximum_density/resolution
+    
+    @interact(density = (0, maximum_density, stepsize))
+    def update(density):
+        with fig.batch_update():
+            waterHeight = cm.CalcEquilibrium(height, density, GLOBAL_WaterDensity, GLOBAL_Gravity)
+            
+            #updated plots
+            fig.data[0].x, fig.data[0].y = cm.GetCubeDiagram(length, height, waterHeight)
+            fig.data[1].x, fig.data[1].y = cm.GetWaterDiagram(length, waterHeight)
+            
+            #fig.data[0].visible = True
+            #fig.data[1].visible = True
+        return fig
     return None
 
 def RotatedCubeGraph(length = 5, depth = 5, height = 5, density = 0.5, rotation = 45,resolution = 32):
@@ -73,11 +115,11 @@ def GenerateBoatGraph(width, height, length, c, a, b, type, resolution = 32):
     An interactive graph with a slider for weight. Shows where the equilibrium of the boat is depending on weight, 
     along with a side view of said boat with a line at equilibrium level.
     """
-    X = pf.x_coordinates(length, a)
-    Y = pf.y_coordinates(width , c, a, b, type)
-    Z = pf.z_coordinates(height, c, a, b, type)
+    #X = pf.x_coordinates(length, a)
+    #Y = pf.y_coordinates(width , c, a, b, type)
+    #Z = pf.z_coordinates(height, c, a, b, type)
     
-    heightNormal = eq.GenerateVectorList(X,Y,Z)
+    #heightNormal = eq.GenerateVectorList(X,Y,Z)
     
     return None
     
