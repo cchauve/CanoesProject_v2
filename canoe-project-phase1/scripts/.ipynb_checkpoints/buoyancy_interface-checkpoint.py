51ac6646dd2b32ba4a8d15df350bf8f285cc195f
Kaj Wilson <47764116+HitchyHawk@users.noreply.github.com>
2022-09-01 00:17:58 -0700

    Deprecated old scripts and methods into archives

--- canoe-project-phase1/scripts/.ipynb_checkpoints/buoyancy_interface-checkpoint.py
+++ canoe-project-phase1/scripts/.ipynb_checkpoints/buoyancy_interface-checkpoint.py
@@ -11,7 +11,6 @@ import ipywidgets as widgets
 from IPython.display import display
 
 import scripts.models.cubeModel as cm
-import scripts.models.rotatingCubeModel as rcm # TO DEPRECATE
 import scripts.models.canoeModel as canoe 
 import scripts.visualization as vs
 import scripts.models.graphics as graphics
@@ -167,94 +166,3 @@ def GenerateBoatGraph(length, width, height, c, canoe_type, resolution = 32):
         return fig
     return None
 
-"""
-===================================
-      T O   D E P R E C A T E
-===================================
-
-def EquilibriumCubeGraph(length = 5, depth = 5, height = 5, resolution = 64):
-    
-    An interactive graph with slider for weight. Shows where the equilibrium of the cube is.
-    Subgraph on the side plotting weight vs equilibrium.
-         
-    fig = go.Figure()
-    
-    fig.update_layout(
-        showlegend = False,
-        width = 500,
-        height = 700
-    )
-    fig.update_xaxes(range = [length  * (-1/4), length * 5/4])
-    fig.update_yaxes(range = [height  * (-5/4), height * 5/4], scaleanchor = "x", scaleratio = 1)
-    
-    ##initial plot
-    waterHeight = cm.CalcEquilibrium(height, 0, GLOBAL_WaterDensity)
-    
-    traceCube = cm.GetCubeTrace(length, height, waterHeight)
-    traceWater = cm.GetWaterTrace(length, waterHeight)
-    
-    fig.add_trace(traceCube)
-    fig.add_trace(traceWater)
-    
-    fig.data[0].visible = True
-    fig.data[1].visible = True
-    #fig.show()
-    
-    maximum_density = GLOBAL_WaterDensity
-    stepsize = maximum_density/resolution
-    
-    @interact(density = (0, maximum_density, stepsize))
-    def update(density):
-        with fig.batch_update():
-            waterHeight = cm.CalcEquilibrium(height, density, GLOBAL_WaterDensity)
-            
-            fig.data[0].x, fig.data[0].y = cm.GetCubeDiagram(length, height, waterHeight)
-            fig.data[1].x, fig.data[1].y = cm.GetWaterDiagram(length, waterHeight)
-            
-            waterMass = cm.CalcBouyancy(length, depth, height, waterHeight, GLOBAL_WaterDensity, 1)
-            cubeMass = cm.CalcWeight(length, depth, height, density, 1)
-            fig.update_layout(
-                title = "Cube Mass: " + "{:.4f}".format(cubeMass) + " Equilibrium level: " + "{:.4f}".format(waterHeight)
-            )
-        return fig
-    return None
-    
-def RotatedCubeGraph(length = 1, depth = 1, height = 1, density = 500, resolution = 32):
-    
-    An interactive graph with slider for water level. 
-    length, depth, height, density. Rotation will be in degrees (will be converted to radians in script) 
-    resolution of normals 
-   
-    fig = go.Figure()
-    fig.update_layout(
-        showlegend = False,
-        width = 500,
-        height = 700
-    )
-    maxRange = np.multiply([-1,1],max(length,height) * np.sqrt(2)*1.5)
-    fig.update_xaxes(range = maxRange)
-    fig.update_yaxes(range = maxRange, scaleanchor = "x", scaleratio = 1)
-    
-    theta = 0
-    
-    traceCube, traceWater = rcm.GetTrace(length, height, theta)
-    fig.add_trace(traceCube)
-    fig.add_trace(traceWater)
-    fig.data[0].visible = True
-    fig.data[1].visible = True
-    
-    @interact(theta = (0, np.pi, np.pi/resolution))
-    def update(theta):
-        with fig.batch_update():
-            X_cube, Y_cube, X_water, Y_water = rcm.GetDiagram(length, height, theta)
-            fig.data[0].x, fig.data[0].y = X_cube, Y_cube
-            fig.data[1].x, fig.data[1].y = X_water, Y_water
-            
-            volume = rcm.GetArea(X_water, Y_water) * depth
-            fig.update_layout(
-                title = "Displaced Water Volume: " + "{:.4f}".format(volume)
-            )
-        return fig
-    
-    return None
-"""
