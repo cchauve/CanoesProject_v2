721e70c25801effe441a654977bcada15908e192
Kaj Wilson <47764116+HitchyHawk@users.noreply.github.com>
2022-11-17 08:54:04 -0800

    Bezier Integration
    Not quite working for buoyancy.
    Working out some indexing errors

--- canoe-project-phase1/scripts/buoyancy_interface.py
+++ canoe-project-phase1/scripts/buoyancy_interface.py
@@ -26,43 +26,59 @@ def CubeGraph():
     set up sliders, labels, and ui position. As well as calling the the method to display everything
     """
     stepSize = 0.05
-    length = widgets.FloatSlider(min = 0.01, 
-                                 max = 2, 
-                                 step = stepSize, 
-                                 value = 1, 
-                                 description = "length (m): ", 
-                                 continuous_update = False)
+    length = widgets.FloatSlider(
+        min = 0.01,                                  
+        max = 2,                          
+        step = stepSize,                                 
+        value = 1,                              
+        description = "length (m): ",                      
+        continuous_update = False
+    )
     
-    width  = widgets.FloatSlider(min = 0.01, 
-                                 max = 2, 
-                                 step = stepSize, 
-                                 value = 1, 
-                                 description = "width (m): ", 
-                                 continuous_update = False)
+    width  = widgets.FloatSlider(
+        min = 0.01,                                
+        max = 2,                               
+        step = stepSize,                              
+        value = 1,                                 
+        description = "width (m): ",                            
+        continuous_update = False
+    )
     
-    height = widgets.FloatSlider(min = 0.01, 
-                                 max = 2, 
-                                 step = stepSize, 
-                                 value = 1, 
-                                 description = "height (m): ", 
-                                 continuous_update = False)
+    height = widgets.FloatSlider(
+        min = 0.01,                                
+        max = 2,                          
+        step = stepSize,                            
+        value = 1,                          
+        description = "height (m): ",        
+        continuous_update = False
+    )
     
-    density = widgets.FloatSlider(min = 0.1, 
-                                  max = GLOBAL_WaterDensity * 1.05, 
-                                  value = 750 , 
-                                  step = stepSize, 
-                                  description = "density: ", 
-                                  continuous_update = False)
+    density = widgets.FloatSlider(
+        min = 0.1,                         
+        max = GLOBAL_WaterDensity * 1.05,                              
+        value = 750 ,                                
+        step = stepSize,                                 
+        description = "density: ",                            
+        continuous_update = False
+    )
     
     density.style.handle_color = "#141414"
     
-    ui = widgets.TwoByTwoLayout(top_left = length, top_right = width, bottom_left = height, bottom_right = density)
-    out = widgets.interactive_output(WaterLevelCubeGraph, 
-                                     {"length": length, 
-                                      "width": width, 
-                                      "height": height, 
-                                      "density": density, 
-                                      "resolution": fixed(64)})
+    ui = widgets.TwoByTwoLayout(
+        top_left = length, 
+        top_right = width, 
+        bottom_left = height, 
+        bottom_right = density
+    )
+    
+    out = widgets.interactive_output(
+        WaterLevelCubeGraph,                           
+        {"length": length,                                    
+         "width": width,                                   
+         "height": height,                                     
+         "density": density,              
+         "resolution": fixed(64)}
+    )
     display(ui, out)
 
 def CanoeGraph():
@@ -71,47 +87,62 @@ def CanoeGraph():
     set up sliders, labels, and ui position. As well as calling the the method to display everything
     """
     stepSize = 0.05
+    widgetLength = widgets.FloatSlider(
+        min   = 4, 
+        max   = 10, 
+        step  = stepSize, 
+        value = 7.687, 
+        description = "length (m)", 
+        continuous_update = False
+    )
     
-    length = widgets.FloatSlider(min = 0.01, 
-                                 max = 5, 
-                                 step = stepSize, 
-                                 value = 4, 
-                                 description = "length (m)", 
-                                 continuous_update = False)
+    widgetWidth = widgets.FloatSlider(
+        min   = 0.1, 
+        max   = 3, 
+        step  = stepSize, 
+        value = 1.250, 
+        description = "width (m)", 
+        continuous_update = False
+    )
     
-    width  = widgets.FloatSlider(min = 0.01, 
-                                 max = 1.5, 
-                                 step = stepSize, 
-                                 value = 0.5, 
-                                 description = "width (m)", 
-                                 continuous_update = False)
+    widgetHeight = widgets.FloatSlider(
+        min   = 0.1, 
+        max   = 3, 
+        step  = stepSize, 
+        value = 1.135, 
+        description = "height (m)", 
+        continuous_update = False
+    )
     
-    height = widgets.FloatSlider(min = 0.01, 
-                                 max = 1.5, 
-                                 step = stepSize, 
-                                 value = 0.5, 
-                                 description = "height (m)", 
-                                 continuous_update = False)
+    widgetNames = widgets.Dropdown(
+        options = [("Nootkan-Style Canoe", 1), ("Haida-Dugout Canoe", 2)],
+        value = 1,
+        description = "Canoe type: "
+    )
+    
+    "STARTS THE INTERACTABLE GRAPH"
+    widgets.interact_manual(
+        GenerateBoatGraph, 
+        length     = widgetLength, 
+        width      = widgetWidth, 
+        height     = widgetHeight, 
+        canoe_type = widgetNames
+    )
+    
 
-    curveDef = widgets.FloatSlider(min = 0.01, 
-                                   max = 2, 
-                                   step = stepSize, 
-                                   value = 0.5, 
-                                   description = "curve definition", 
-                                   continuous_update = False, 
-                                   orientation = "vertical")
+    #ui = widgets.GridspecLayout(4, 2, width = "50%", height = "275px")
+    #ui[0, 0] = length
+    #ui[1, 0] = width
+    #ui[2, 0] = height
+    #ui[3, :] = canoeType
     
-    canoeType = widgets.IntSlider(min = 1, max = 3, description = "Canoe Type", layout=widgets.Layout(width='85%'))
-
-    ui = widgets.GridspecLayout(4, 2, width = "50%", height = "275px")
-    ui[0, 0] = length
-    ui[1, 0] = width
-    ui[2, 0] = height
-    ui[0:2, 1] = curveDef
-    ui[3, :] = canoeType
-    
-    out = widgets.interactive_output(GenerateBoatGraph, {"length": length, "width": width, "height": height, "c": curveDef, "canoe_type": canoeType, "resolution": fixed(50)})
-    display(ui, out)
+    #out = widgets.interactive_output(GenerateBoatGraph, 
+    #                                 {"length": length, 
+    #                                  "width": width, 
+    #                                  "height": height, 
+    #                                  "canoe_type": canoeType, 
+    #                                  "resolution": fixed(50)})
+    #display(ui, out)
 
 def figureSetup(traces, width, height, showlegend = False, xRange = None, yRange = None):
     """
@@ -157,7 +188,11 @@ def WaterLevelCubeGraph(length = 5, width = 5, height = 5, density = 0.5, resolu
     fig = figureSetup([traceCube, traceWater, traceArrow], width = 800, height = 500, xRange = xRange, yRange = yRange)
     
     #widget set up
-    waterLevelWidget = widgets.FloatSlider(min = 0, max = height, step = stepsize, description = "depth (m)", layout = widgets.Layout(width = "50%"))
+    waterLevelWidget = widgets.FloatSlider(min = 0, 
+                                           max = height, 
+                                           step = stepsize, 
+                                           description = "depth (m)", layout = widgets.Layout(width = "50%"))
+    
     waterLevelWidget.style.handle_color = "lightblue"
     @interact(waterLevel = waterLevelWidget)
     def update(waterLevel):
@@ -170,7 +205,8 @@ def WaterLevelCubeGraph(length = 5, width = 5, height = 5, density = 0.5, resolu
             arrowMagnitude = (totalForce / (maximumForce)) * height * 1.2
             fig.data[2].x, fig.data[2].y = graphics.GetArrow(arrowX, arrowY, [0,1], arrowMagnitude, arrowMagnitude/2)
             
-            titleString =  "Cuboid weight:" + "{:.2f}".format(cm.CalcWeight(length, width, height, density, gravity = GLOBAL_Gravity)) + "(N)"
+            titleString  = "Cuboid weight: " 
+            titleString += "{:.2f}".format(cm.CalcWeight(length, width, height, density, gravity = GLOBAL_Gravity)) + "(N)"
             titleString += " Depth: " + "{:.2f}".format(waterLevel) + "(m)"
             titleString += " Total force: " + "{:.2f}".format(totalForce) + "(N)"
             fig.update_layout(
@@ -179,27 +215,42 @@ def WaterLevelCubeGraph(length = 5, width = 5, height = 5, density = 0.5, resolu
         return fig
 
 
-def GenerateBoatGraph(length, width, height, c, canoe_type, resolution = 32):
+def GenerateBoatGraph(length, width, height, canoe_type, resolution = 4):
     """ 
     An interactive graph with a slider for weight. Shows where the equilibrium of the boat is depending on weight, 
     along with a side view of said boat with a line at equilibrium level.
     """
     #variable set up
     mass = 100
-    X, Y, Z = canoe.GetXYZ(width, height, length, c, canoe_type, resolution = 32)
+    symmetry = 2
+    
+    X, Y, Z = canoe.GetCanoe(width, height, length, canoe_type, resolution)
+    #arrayDimensions = [len(X), len(X[0])]
     heightNormalArea = eq.GenerateVectorList(X,Y,Z)
-    maxWeight = abs(eq.CalculateForce(heightNormalArea, 0) / GLOBAL_Gravity)
+    
+    maxWeight = abs(eq.CalculateForce(heightNormalArea, 0)*2 / GLOBAL_Gravity)
     stepsize = maxWeight/resolution
     
+    xLine, zLine = getCanoeBorder(X,Z)
+    xRange = [-0.5 * length, 1.5 * length]
+    yRange = [-1.1 * height, 1.1 * height]
     #plot setup
-    levelTrace = canoe.GetLevelTrace(0, length)
-    sideTrace = canoe.GetSideTrace(height, length, c, canoe_type, resolution)
-    sideData = [0]*2 #set up to retrieve side data
-    sideData = canoe.GetSideData(height, length, c, canoe_type, resolution)
-    xRange = [-length * 1.1, length * 1.1] 
-    yRange = [-height * 1.1, height * 1.1]
+    levelTrace = go.Scatter(
+        x = [-0.5*length, 1.5*length], 
+        y = [0, 0],
+        visible = False,
+        line = dict(color = "#1991df", width = 3)
+    )
+   
+    sideTrace = go.Scatter(
+        x = xLine, 
+        y = zLine, 
+        visible = False, 
+        line = dict(color = "#141414", width = 3)
+    )
     
-    fig = figureSetup([sideTrace, levelTrace], width = 700, height = 500, xRange = xRange, yRange = yRange) #Change the list order for traces to change render order
+    #Change the list order for traces to change render order
+    fig = figureSetup([sideTrace, levelTrace], width = 700, height = 500, xRange = xRange, yRange = yRange) 
     
     #widget setup
     massWidget = widgets.FloatSlider(min = 0, max = maxWeight, step = stepsize, description = "mass (kg)", value = maxWeight)
@@ -209,8 +260,8 @@ def GenerateBoatGraph(length, width, height, c, canoe_type, resolution = 32):
     def update(mass):
         with fig.batch_update():
             
-            level = eq.BinarySearch(heightNormalArea, mass * GLOBAL_Gravity)
-            fig.data[0].y = np.add(sideData[1], level)
+            level = eq.BinarySearch(heightNormalArea, mass * GLOBAL_Gravity, symmetryMultiplier = symmetry)
+            fig.data[0].y = np.add(zLine, level)
             
             fig.update_layout(
                 title = "Canoe top to water (m): " + "{:.4f}".format(level)
@@ -218,3 +269,31 @@ def GenerateBoatGraph(length, width, height, c, canoe_type, resolution = 32):
         return fig
     return None
 
+def getCanoeBorder(X,Z):
+    arrayDimensions = [len(X), len(X[0])]
+    size = (arrayDimensions[0] + arrayDimensions[1] - 1)*2
+    xLine = [0]*(size)
+    zLine = [0]*(size)
+    for i in range(0, arrayDimensions[0]-1):
+        xLine[i] = X[i][0]
+        zLine[i] = Z[i][0]
+        
+    offset = arrayDimensions[0] - 1
+    for i in range(0, arrayDimensions[1]-1):
+        xLine[i + offset] = X[arrayDimensions[0]-1][i]
+        zLine[i + offset] = Z[arrayDimensions[0]-1][i]
+        
+    offset += arrayDimensions[1] - 1
+    for i in range(0, arrayDimensions[0]-1):
+        xLine[i + offset] = X[arrayDimensions[0]-1-i][arrayDimensions[1]-1]
+        zLine[i + offset] = Z[arrayDimensions[0]-1-i][arrayDimensions[1]-1]
+        
+    offset += arrayDimensions[0]-1
+    for i in range(0, arrayDimensions[1]-1):
+        xLine[i + offset] = X[0][arrayDimensions[1]-1-i]
+        zLine[i + offset] = Z[0][arrayDimensions[1]-1-i]
+    
+    return X,Z
+        
+        
+    
